29aa2c9512d35b06b98fc36c0f2016a56833bcf2
Andrei Piatsevich <andrei.piatsevich@teamgetter.com>
2022-11-23 13:05:05 +0300

    bootstrap bug fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path: 'login', component: LoginComponent},
+{ path: 'register', component: RegisterComponent},
+{ path: 'user/management', component: UserComponent},
+{ path: '**', redirectTo: '/login', pathMatch: 'full'}
+];
 
 @NgModule({
+  declarations: [],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
